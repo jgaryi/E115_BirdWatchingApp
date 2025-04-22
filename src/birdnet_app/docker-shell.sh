b18d4cb6da5c7795732c833879e4617d91c09c4e
jgaryi <jgaryi@users.noreply.github.com>
2025-04-23 01:34:38 +0200

    Add files via upload

---
+++ src/birdnet_app/docker-shell.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Name of the image and container
+export IMAGE_NAME="birdnet-app"
+export CONTAINER_NAME="birdnet_app"
+export BASE_DIR=$(pwd)
+
+# Make sure the Docker network exists
+docker network inspect cheese-app-network >/dev/null 2>&1 || docker network create cheese-app-network
+
+# Build the image
+docker build -t $IMAGE_NAME -f Dockerfile .
+
+# Run the container
+docker run --rm --name $CONTAINER_NAME -ti \
+  -v "$BASE_DIR":/app \
+  -p 9090:9090 \
+  --network cheese-app-network \
+  $IMAGE_NAME
